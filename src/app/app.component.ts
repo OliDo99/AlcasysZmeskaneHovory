e54a4653b071a4b49ff07c952d68f3a8c853133b
OliDo99 <oliver.dohnal1@gmail.com>
2023-10-13 15:08:19 +0200

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,51 +7,31 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  ngOnInit(){
+  ngOnInit(): void {
     this.getData()
   }
   constructor(private http: HttpClient) {}
   title = 'Test';
   isCollapsed = [false,false,false];
-  show = -1;
-  
+  show = -1
   phones!:any;
-
-  done(phone:number,index:number,){
-    let i = 0;
-    for(let element of this.phones[index].phoneCalls) {
-      if(element.phone == phone) { 
-        this.phones[index].phoneCalls[i].done = !this.phones[index].phoneCalls[i].done;
-        
-      }
-      i++;
-      
-    }
-    console.log(this.phones)
-  }
  
   toggleCollapse(i: number) {
-    if(this.show == i){ this.show =-1; }
-    else{ this.show = i; }
+    if(this.show == i) { this.show =-1; }
+    else{this.show = i;}
   }
 
-  getData(){  
+  getData(){
     this.http.get('http://127.0.0.1:5000')
-    .subscribe((data:any) => { 
+    .subscribe((data:any) => {
       this.phones= data.data
-      console.log(data.data)
     })
   }
 
-  postData(table: string, id: number){  
-    this.http.post('http://127.0.0.1:5000/secret',{"Table":table,"ID":id})
-    .subscribe((data:any) => { 
-      console.log(data)
+  postData(table: string,data: boolean,id: number){
+    this.http.post('http://127.0.0.1:5000/secret',{"Table":table,"Data":data,"ID":id})
+    .subscribe(() => {
+      this.getData()
     })
   }
-  
 }
-
-
-
-
